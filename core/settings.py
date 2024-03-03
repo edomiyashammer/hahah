952a6dc6dd12b9d6af2df0020dc3455b7e4ce9c0
edomiyashammer <edomiashammer@gmail.com>
2024-03-03 15:06:39 +0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['www.giyaamarket.com']
 
 
 # Application definition
